a2561cf7967ababa6c5f71516a7ab54e29d503c7
pawidachum <pawida.may@gmail.com>
2021-03-07 16:28:04 +0700

    ViewChild

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Lab4-Recipe3'`, () => {
+  it(`should have as title 'Lab-Recipe1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Lab4-Recipe3');
+    expect(app.title).toEqual('Lab-Recipe1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Lab4-Recipe3 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Lab-Recipe1 app is running!');
   });
 });
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+products: Product[];
+
+constructor() { 
+  this.products = []; 
+}
+
+ngOnInit() {}
+  
+selectedProduct(product: Product){
+    alert(`Product ${product.name} selected`);
+}
+
+}
