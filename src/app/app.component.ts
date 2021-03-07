1961b94ff81dbe9603c7ea11012e7454fc32c4f1
pawidachum <pawida.may@gmail.com>
2021-03-07 16:27:46 +0700

    ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Lab4-Recipe3';
+
+export class AppComponent implements OnInit{
+  @ViewChild('productList',{static:true})
+  productList: ProductListComponent;
+
+  ngOnInit():void{
+    this.productList.products = [
+      {
+      name: 'ส้มโอ',
+      price: 990
+      },
+      {
+      name: 'แตงโม', 
+      price: 1000
+      },
+      {
+      name: 'มะพร้าวน้ําหอม',
+      price: 500
+      },
+      {
+      name: 'มะม่วง',
+      price: 800
+      }
+    ];
+  }
+
+  constructor() {}
+
+  title = 'Lab4-Recipe2';
 }
